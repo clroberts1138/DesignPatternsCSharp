d98f151df50371f27bece5f3830746d157cca000
Chris Roberts <clroberts1138@gmail.com>
2018-09-18 21:09:38 -0500

    Base files for Design Patterns

---
+++ DesignPatternsClass/DesignPatternsClass/Base/CrossCountry.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsClass
+{
+    public class CrossCountry : AbstractMountainBike
+    {
+
+        public CrossCountry()
+            : this(BikeColor.Green)
+        {
+
+        }
+
+        public CrossCountry(BikeColor color)
+            : base(color)
+        {
+
+        }
+    }
+}
---
+++ DesignPatternsClass/DesignPatternsClass/Base/DownHill.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsClass
+{
+    public class DownHill : AbstractMountainBike
+    {
+        public DownHill()
+            : this(BikeColor.Red)
+        {
+
+        }
+
+        public DownHill(BikeColor color)
+            : base(color)
+        {
+
+        }
+    }
+}
